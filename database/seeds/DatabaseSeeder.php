7bab30866712e309b802ca19c785b46c114b2fc9
James0251 <den4643@yandex.ru>
2021-03-10 21:15:40 +0300

    Написал seeders и factory для таблиц блога в БД. заполнил таблицы faker-данными.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,8 +9,23 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
+    public function run() {
+        $this->call(UserTableSeeder::class);
+        $this->command->info('Таблица пользователей загружена данными!');
+
+        $this->call(CategoryTableSeeder::class);
+        $this->command->info('Таблица категорий загружена данными!');
+
+        $this->call(TagTableSeeder::class);
+        $this->command->info('Таблица тегов загружена данными!');
+
+        $this->call(PostTableSeeder::class);
+        $this->command->info('Таблица постов загружена данными!');
+
+        $this->call(PostTagTableSeeder::class);
+        $this->command->info('Таблица пост-тег загружена данными!');
+
+        $this->call(CommentTableSeeder::class);
+        $this->command->info('Таблица комментариев загружена данными!');
     }
 }
