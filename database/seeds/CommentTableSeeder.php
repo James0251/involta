f7e38ba29b1f78c6f0a55f6bfbcb703c9bf5c842
James0251 <den4643@yandex.ru>
2021-03-10 21:08:41 +0300

    Создал необходимые seeder миграции для блога

---
+++ database/seeds/CommentTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CommentTableSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        // создать 200 комментариев
+        factory(App\Comment::class, 200)->create();
+    }
+}
