7e9ea4c1eeaa8279f2eaad2ce006e587a7ee9b02
James0251 <den4643@yandex.ru>
2021-03-11 14:57:13 +0300

    Вывел все посты Пользователей по определенному тэгу, а так же все посты Автора

--- resources/views/blog/author.blade.php
+++ resources/views/blog/author.blade.php
@@ -1,18 +1,3 @@
-{{-- Для показа списка всех постов с тегом --}}
-{{--@extends('layout.site', ['title' => 'Посты с тегом: ' . $tag->name])--}}
-
-{{--@section('content')--}}
-{{--    <h1 class="mb-3">Посты с тегом: {{ $tag->name }}</h1>--}}
-{{--    @foreach ($posts as $post)--}}
-{{--        @include('blog.part.post', ['post' => $post])--}}
-{{--    @endforeach--}}
-{{--    {{ $posts->links() }}--}}
-{{--@endsection--}}
-
-
-
-
-
 {{-- Для показа списка постов одного автора --}}
 @extends('layout.site', ['title' => 'Посты автора: ' . $user->name])
 
